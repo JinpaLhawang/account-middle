d48991da1ef32a84ccd216fda024148be4052173
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-04-20 17:07:37 -0400

    Turned into rest controller and added hello world endpoint. Also added logging.

--- src/main/java/com/jambudvipa/PropertiesApplication.java
+++ src/main/java/com/jambudvipa/PropertiesApplication.java
@@ -1,10 +1,23 @@
 package com.jambudvipa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PropertiesApplication {
+
+  private static final Logger log = LoggerFactory.getLogger(PropertiesApplication.class);
+
+  @RequestMapping("/")
+  public String home() {
+    log.info("Hello World!");
+    return "Hello World!";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(PropertiesApplication.class, args);
